f79981e128fed31e8c877d69f5cf3dc9d073b247
za-king <muhammadzakysyukur@gmail.com>
2023-11-29 07:00:38 +0700

    add login style

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,34 +1,41 @@
-import {auth , provider} from "../config/firebase-config"
-import {signInWithPopup} from "firebase/auth"
+import { auth, provider } from "../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 const Auth = () => {
+  const navigate = useNavigate();
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const userInfo = {
+        userID: result.user.uid,
+        name: result.user.displayName,
+        profilePhoto: result.user.photoURL,
+        userEmail: result.user.email,
+        isAuth: true,
+      };
 
-    const navigate = useNavigate()
-    const signInWithGoogle = async() =>{
-        try {
-          const result = await signInWithPopup(auth, provider)
-        const userInfo = {
-            userID : result.user.uid,
-            name : result.user.displayName,
-            profilePhoto : result.user.photoURL,
-            userEmail : result.user.email,
-            isAuth : true
-        }
+      localStorage.setItem("auth", JSON.stringify(userInfo));
 
-        
-        localStorage.setItem("auth" , JSON.stringify(userInfo))
-
-        navigate("/dashboard")
-        } catch (error) {
-          console.log(error)
-        }
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    
   return (
-    <div>
-      <p>Sign In With Goole to Continue</p> 
-      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="border-2 rounded-lg p-12 text-center">
+        <p className="text-xl font-bold">Sign In With Goole to Continue</p>
+
+        <button
+          className="flex items-center gap-2 border rounded p-4 mx-auto my-4"
+          onClick={signInWithGoogle}
+        >
+          <FcGoogle />
+          Sign In With Google
+        </button>
+      </div>
     </div>
   );
 };
