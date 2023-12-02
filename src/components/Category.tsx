e3a1a42afd71f76f9005dc12661e273f23ff1e23
za-king <muhammadzakysyukur@gmail.com>
2023-12-03 02:57:07 +0700

    add cat

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -26,7 +26,7 @@ const Category = () => {
       <div className="grid grid-cols-1 md:grid-cols-6 mx-4 justify-between justify-items-center gap-2 my-12">
         {categoryList.map((item: any) => {
           return (
-            <div className={`${handleColor(item.name)} w-full h-12 rounded  grid justify-center items-center text-xs font-bold`}>
+            <div className={`${handleColor(item.name)} w-full h-12 rounded  grid justify-center items-center text-xs font-bold cursor-pointer`}>
               {item.name}
             </div>
           );
